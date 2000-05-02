31549063b308b2ef628414436db9a4ca73e6a4d9
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2000-05-02 21:46:31 +0000

    usage of 256 bit keys, huge code cleanup, checking of most possible error conditions like timeout reading, no memory and so on
    git-svn-id: http://svn.23.nu/svn/repos/ddns/trunk@98 2d70b15c-62de-0310-b45c-a2b26695ab02

--- ddnsd.c
+++ ddnsd.c
@@ -1,10 +1,16 @@
-/* $Id: ddnsd.c,v 1.11 2000/05/01 11:47:18 drt Exp $
+/* $Id: ddnsd.c,v 1.12 2000/05/02 21:46:31 drt Exp $
  *
- * server for ddns
+ * server for ddns - this file is to long
  * 
  * $Log: ddnsd.c,v $
+ * Revision 1.12  2000/05/02 21:46:31  drt
+ * usage of 256 bit keys,
+ * huge code cleanup,
+ * checking of most possible error conditions
+ * like timeout reading, no memory and so on
+ *
  * Revision 1.11  2000/05/01 11:47:18  drt
- * ttl/leasetime coms now from data.cdb
+ * ttl/leasetime comes now from data.cdb
  *
  * Revision 1.10  2000/04/30 23:03:18  drt
  * Unknown user is now communicated by using uid == 0
@@ -45,7 +51,7 @@
 #include <netdb.h>     /* gethostbyname */
 #include <stdio.h>     /* rename */
 #include <stdlib.h>    /* random, clock */
-#include <sys/stat.h>  /* stat */
+#include <sys/stat.h>  /* stat, umask */
 #include <sys/types.h> /* utimbuf */
 #include <time.h>      /* time */
 #include <unistd.h>    /* close, getpid, getppid, sleep, unlink */
@@ -53,6 +59,7 @@
 
 #include "buffer.h"
 #include "cdb.h"
+#include "droproot.h"
 #include "env.h"
 #include "error.h"
 #include "fmt.h"
@@ -69,14 +76,19 @@
 
 #include "ddns.h"
 
-static char rcsid[] = "$Id";
+static char rcsid[] = "$Id: ddnsd.c,v 1.12 2000/05/02 21:46:31 drt Exp $";
 
-static char datadir[] = "dnsdata/cx/rc23/walledcity/dyn";
+static char *datadir;
 
 static uint32 ttl;
 
 static unsigned char *remotehost, *remoteinfo, *remoteip, *remoteport;
-static stralloc username = { 0 };
+static stralloc username = {0};
+
+void nomem(void)
+{
+  strerr_die1sys(111, "ddnsd: fatal: help - no memory ");
+}
 
 /* fill p with random, timestamp, magic, encrypt it and send it */
 void ddnsd_send(struct ddnsreply *p)
@@ -113,8 +125,8 @@ void ddnsd_send(struct ddnsreply *p)
 
 void ddnsd_log(uint32 uid, char *str)
 {
- char strnum[FMT_ULONG];
-
+  char strnum[FMT_ULONG];
+  
   /* Do logging */
   buffer_puts(buffer_2, remotehost);
   buffer_puts(buffer_2, " [");
@@ -134,29 +146,29 @@ void ddnsd_log(uint32 uid, char *str)
 /* Sends an error packet to the client and logs an error */
 void ddnsd_send_err(uint32 uid, uint16 errtype, char *errstr)
 {
-  struct ddnsreply p = { 0 };
+  struct ddnsreply p;
   
   p.type = errtype;
   p.uid = uid;
-
+  
   /* leasetime is unused when sending error packets so we fill it with random data */
   p.leasetime.x = (uint64) (randomMT() & (uint64)((uint64)randomMT() << 32));
-
+  
   ddnsd_log(uid, errstr);
- 
+  
   ddnsd_send(&p);
-
+  
   exit(111);
 }
 
 void ddnsd_send_err_sys(uint32 uid, char *errstr)
 {
-  stralloc err = { 0 };
+  stralloc err = {0};
 
-  stralloc_copys(&err, errstr);
-  stralloc_cats(&err, ": ");
-  stralloc_cats(&err, error_str(errno));
-  stralloc_0(&err);
+  if(!stralloc_copys(&err, errstr)) nomem();
+  if(!stralloc_cats(&err, ": ")) nomem();
+  if(!stralloc_cats(&err, error_str(errno))) nomem();
+  if(!stralloc_0(&err)) nomem();
   ddnsd_send_err(uid, DDNS_T_ESERVINT, err.s);
 }
 
@@ -183,7 +195,7 @@ void dump_packet(struct ddnsrequest *p, char *info)
 }
 
 /* returns 1 when found, 0 when unknown user */
-int ddnsd_find_user(struct ddnsrequest *p, stralloc *sa)
+int ddnsd_find_user(uint32 uid, stralloc *sa)
 {
   struct cdb c;
   int r;
@@ -193,36 +205,34 @@ int ddnsd_find_user(struct ddnsrequest *p, stralloc *sa)
   fd = open_read("data.cdb");
   if (fd == -1) 
     {
-      ddnsd_send_err_sys(p->uid, "can't open data.cdb");
+      ddnsd_send_err_sys(uid, "can't open data.cdb");
     }
   
   cdb_init(&c, fd);
 
   /* find entry */
   /* uid in cdb is in network byte order */ 
-  uint32_pack(key, p->uid);
+  uint32_pack(key, uid);
   r = cdb_find(&c, key, 4); 
   if (r == 1)
     {
       /* read data */
-      stralloc_ready(sa, cdb_datalen(&c));
+      if(!stralloc_ready(sa, cdb_datalen(&c))) nomem();
       if (cdb_read(&c, sa->s, cdb_datalen(&c), cdb_datapos(&c)) == -1)
-	{
-	  ddnsd_send_err_sys(p->uid, "can't read from data.cdb");
-	}
+	ddnsd_send_err_sys(uid, "can't read from data.cdb");
       else
-	{
-	  sa->len = cdb_datalen(&c);
-	}
+	sa->len = cdb_datalen(&c);
     }
   else
-    {
-      /* can't find id */
-      return 0;
-    }
+    /* can't find id */
+    return 0;
 
   cdb_free(&c);
   close(fd);
+
+  /* did ddnsd-data create crap? */
+  if(sa->len < 37)
+    return 0;
 
   return 1;
 }
@@ -233,22 +243,23 @@ int ddnsd_recive(struct ddnsrequest *p)
 {
   int r;
   char *key;
-  struct ddnsrequest ptmp = { 0 };
-  stralloc data = { 0 };
-
+  struct ddnsrequest ptmp;
+  stralloc data = {0};
+  
   r = timeoutread(60, 0, &ptmp, sizeof(struct ddnsrequest));
-  /* XXX: check result */
+  if(r != 68) 
+    ddnsd_send_err(p->uid, DDNS_T_EPROTERROR, "wrong packetsize/timeout");
 
   uint32_unpack((char*) &ptmp.uid, &p->uid);
   
-  if(ddnsd_find_user(p, &data))
+  if(ddnsd_find_user(p->uid, &data))
     {
       key = data.s;
-      uint32_unpack(&data.s[16], &ttl);
-      stralloc_copyb(&username, &data.s[20], data.len-20);
+      uint32_unpack(&data.s[32], &ttl);
+      if(!stralloc_copyb(&username, &data.s[36], data.len-36)) nomem();
       
-      /* initialize rijndael with 256 bit blocksize and 128 bit keysize */
-      rijndaelKeySched(8, 4, key);
+      /* initialize rijndael with 256 bit blocksize and 256 bit keysize */
+      rijndaelKeySched(8, 8, key);
       
       /* decrypt with rijndael */
       rijndaelDecrypt((char *) &ptmp.type);
@@ -265,78 +276,79 @@ int ddnsd_recive(struct ddnsrequest *p)
       ptmp.loc_vpre = p->loc_hpre;
       ptmp.loc_hpre = p->loc_vpre;
       
-      dump_packet(p, "entpackt");
-      
       if(p->magic != DDNS_MAGIC)
-	{
-	  ddnsd_send_err(p->uid, DDNS_T_EWRONGMAGIC, "wrong magic");
-	}
+	ddnsd_send_err(p->uid, DDNS_T_EWRONGMAGIC, "wrong magic");
       
       return p->type;
     }
   else
-    {      
-      ddnsd_send_err(0, DDNS_T_EUNKNOWNUID, "unknown user");
-    }
+    ddnsd_send_err(0, DDNS_T_EUNKNOWNUID, "unknown user");
+  
+  return 0;
+}
 
+/* take an username and create a filename from it by 
+   prepending datadir, return it in \0 terminated tmpname */
+void create_datafilename(stralloc *tmpname, stralloc *username)
+{
+  /* create the filename in our datastructure */
+  if(!stralloc_copys(tmpname, datadir)) nomem();
+  if(!stralloc_cats(tmpname, "/")) nomem();
+  if(!stralloc_cat(tmpname, username)) nomem();
+  if(!stralloc_0(tmpname)) nomem();
 }
 
 /* handle a setentryrequest */
 void ddnsd_setentry( struct ddnsrequest *p)
 {
-  struct ddnsreply r = { 0 };
-  struct stat st = {0};
+  struct ddnsreply r;
+  struct stat st;
   char host[64] = {0};
   int loop = 0;
   int fd = 0;
-  stralloc tmpname = { 0 };
-  stralloc err = { 0 };
-  stralloc finname = { 0 };
+  stralloc tmpname = {0};
+  stralloc err = {0};
+  stralloc finname = {0};
   char outbuf[BUFFER_OUTSIZE];
   char strnum[FMT_ULONG];
   char strip[IP4_FMT];
   buffer ssout;
-
   
   /* create a temporary name */
   host[0] = 0;
   gethostname(host,sizeof(host));
   for (loop = 0;;++loop)
     {
-      stralloc_copys(&tmpname, "tmp/");
-      stralloc_catulong0(&tmpname, now(),0); 
-      stralloc_cats(&tmpname, "."); 
-      stralloc_catulong0(&tmpname, getpid(), 0); 
-      stralloc_cats(&tmpname, "."); 
-      stralloc_cats(&tmpname, host); 
-      stralloc_cats(&tmpname, "-"); 
-      stralloc_cat(&tmpname, &username); 
-      stralloc_0(&tmpname);
+      if(!stralloc_copys(&tmpname, "tmp/")) nomem();
+      if(!stralloc_catulong0(&tmpname, now(),0)) nomem(); 
+      if(!stralloc_cats(&tmpname, ".")) nomem(); 
+      if(!stralloc_catulong0(&tmpname, getpid(), 0)) nomem(); 
+      if(!stralloc_cats(&tmpname, ".")) nomem(); 
+      if(!stralloc_cats(&tmpname, host)) nomem(); 
+      if(!stralloc_cats(&tmpname, "-")) nomem(); 
+      if(!stralloc_cat(&tmpname, &username)) nomem(); 
+      if(!stralloc_0(&tmpname)) nomem();
       
-      if (stat(tmpname.s,&st) == -1) if (errno == error_noent) break;
+      if (stat(tmpname.s,&st) == -1) 
+	if (errno == error_noent) 
+	  break;
       /* really should never get to this point */
-      if (loop == 2) _exit(1); /* XXX: logging */
+      if (loop == 2) 
+	_exit(1); /* XXX: logging */
       sleep(1);
     }
-	  
-
+  
   /* create the final name */
-  stralloc_copys(&finname, datadir);
-  stralloc_cats(&finname, "/");
-  stralloc_cat(&finname, &username);
-  stralloc_0(&finname);
-    
-
+  create_datafilename(&finname, &username);
+  
   /* open tmpfile and write to it */
   fd = open_excl(tmpname.s);
-
+  
   if(fd == -1)
-    {
-      ddnsd_send_err_sys(p->uid, tmpname.s);
-    }
+    ddnsd_send_err_sys(p->uid, tmpname.s);
   
   /* XXX: we need checks if the user is comming from the claimed ip */
-
+  
   /* write data to file */
   buffer_init(&ssout,write, fd, outbuf, sizeof outbuf);
   buffer_puts(&ssout, "=");
@@ -345,22 +357,19 @@ void ddnsd_setentry( struct ddnsrequest *p)
   buffer_put(&ssout, strnum, fmt_xlong(strnum, p->uid));
   buffer_puts(&ssout, "\n");
   buffer_flush(&ssout); 
-
+  /* XXX: LOC and IPv6 are missing */
+  
   close(fd);
-
+  
   /* test if the name we are asked to move to is already there */
   if (stat(finname.s, &st) == -1) 
     {
       if(errno != ENOENT)
-	{
-	  ddnsd_send_err_sys(p->uid, finname.s);
-	}
-      /* else: everything is fine, the File doesn't exist */
+	ddnsd_send_err_sys(p->uid, finname.s);
+	/* else: everything is fine, the File doesn't exist */
     }
   else
-    {
-      ddnsd_send_err(p->uid, DDNS_T_EALLREADYUSED, "allready registered");
-    }
+    ddnsd_send_err(p->uid, DDNS_T_EALLREADYUSED, "allready registered");
 
   /* move tmp file to final file */
 
@@ -368,17 +377,17 @@ void ddnsd_setentry( struct ddnsrequest *p)
   /* Yes, but the only harm this race can cause ist that a user 
      gets DDNS_T_ESERVINT instead of DDNS_T_EALLREADYUSED 
   */
-
+  
   if(rename(tmpname.s, finname.s) != 0)
     {
       /* try to delete tmpfile */
       unlink(tmpname.s);
 
       /* return error */
-      stralloc_copys(&err, "can't rename ");
-      stralloc_cats(&err, tmpname.s);
-      stralloc_cats(&err, " to ");
-      stralloc_cat(&err, &finname);
+      if(!stralloc_copys(&err, "can't rename ")) nomem();
+      if(!stralloc_cats(&err, tmpname.s)) nomem();
+      if(!stralloc_cats(&err, " to ")) nomem();
+      if(!stralloc_cat(&err, &finname)) nomem();
       ddnsd_send_err_sys(p->uid, err.s);
     }
   
@@ -392,73 +401,62 @@ void ddnsd_setentry( struct ddnsrequest *p)
   ddnsd_send(&r);  
 }
 
-/* handle a setentryrequest */
+/* handle a renewentry request by updating th ctime of the file */
 void ddnsd_renewentry( struct ddnsrequest *p)
 {
-  struct ddnsreply r = { 0 };
-  stralloc tmpname = { 0 };
   struct stat st = {0};
   struct utimbuf ut;
-
-  /* create the final name */
-  stralloc_copys(&tmpname, datadir);
-  stralloc_cats(&tmpname, "/");
-  stralloc_cat(&tmpname, &username);
-  stralloc_0(&tmpname);
+  struct ddnsreply r;
+  stralloc tmpname = {0};
+  
+  /* create the filename in our datastructure */
+  create_datafilename(&tmpname, &username);
 
   if(stat(tmpname.s, &st) == -1)
     {
       if(errno == ENOENT)
-	{
 	  // File not found
-	  ddnsd_send_err(p->uid, DDNS_T_ENOENTRYUSED, "entry can't be renewed");
-	}
+	  ddnsd_send_err(p->uid, DDNS_T_ENOENTRYUSED, "entry can't be stat()ed");
       else
-	{
-	  ddnsd_send_err_sys(p->uid, tmpname.s);
-	}
+	ddnsd_send_err_sys(p->uid, tmpname.s);
     }
-
+  
+  /* update ctime */
   ut.actime = st.st_atime;
   ut.modtime = st.st_mtime;
-  utime(tmpname.s, &ut);
 
+  if(utime(tmpname.s, &ut) == -1)
+    ddnsd_send_err_sys(p->uid, tmpname.s);
+    
   ddnsd_log(p->uid, "renewing entry");
 
   /* construct the answer Packet */
   r.type = DDNS_T_ACK;
   r.uid = p->uid;
-  /* there should be some more intelligence in setting leasetime */
-  r.leasetime.x = ttl;	 
+  r.leasetime.x = ttl;
   ddnsd_send(&r);  
 }
 
+/* the user requested to delete his entry from the dns */
 void ddnsd_killentry( struct ddnsrequest *p)
 {
-  struct ddnsreply r = { 0 };
-  stralloc tmpname = { 0 };
-
-  /* create the final name */
-  stralloc_copys(&tmpname, datadir);
-  stralloc_cats(&tmpname, "/");
-  stralloc_cat(&tmpname, &username);
-  stralloc_0(&tmpname);
-
+  struct ddnsreply r;
+  stralloc tmpname = {0};
+  
+  /* create the filename in our datastructure */
+  create_datafilename(&tmpname, &username);
+  
   if(unlink(tmpname.s) == -1)
     {
       if(errno == ENOENT)
-	{
-	  // File not found
-	  ddnsd_send_err(p->uid, DDNS_T_ENOENTRYUSED, "entry can't be killed");
-	}
+	// File not found
+	ddnsd_send_err(p->uid, DDNS_T_ENOENTRYUSED, "entry can't be killed");
       else
-	{
-	  ddnsd_send_err_sys(p->uid, tmpname.s);
-	}
+	ddnsd_send_err_sys(p->uid, tmpname.s);
     }
-
+  
   ddnsd_log(p->uid, "killing entry");
-
+  
   /* construct the answer Packet */
   r.type = DDNS_T_ACK;
   r.uid = p->uid;
@@ -466,12 +464,19 @@ void ddnsd_killentry( struct ddnsrequest *p)
   ddnsd_send(&r);  
 }
 
-int main()
+void usage(void)
+{
+  ddnsd_send_err_sys(0, "ddnsd: usage: ddnsd /datadir");
+}
+
+int main(int argc, char **argv)
 {
   struct ddnsrequest p = { 0 };
   
   /* chroot() to $ROOT and switch to $UID:$GID */
-  //  droproot("dffingerd: ");
+  droproot("ddnsd: ");
+
+  umask(024);
 
   /* seed some entropy into the MT */
   seedMT((long long) getpid () *
@@ -491,31 +496,34 @@ int main()
   remoteport = env_get("TCPREMOTEPORT");
   if (!remoteport) remoteport = "unknown";
   
+  datadir = argv[1];
+  if (!datadir) usage();
+
   /* now: 
      remotehost is the remote hostname or "unknown" 
      remoteinfo is some ident string or "-"
      remoteip is the remote ipadress or "unknown" (?)
   */
 
+  /* read one ddns packet from stdin which should be 
+     connected to the tcpstream */
   ddnsd_recive(&p);
   
   switch(p.type)
     {
     case DDNS_T_SETENTRY:
-      ddnsd_log(p.uid, "setentry request");
       ddnsd_setentry(&p);
       break;
     case DDNS_T_RENEWENTRY:
-      ddnsd_log(p.uid, "renewentry request");
       ddnsd_renewentry(&p);
       break;
     case DDNS_T_KILLENTRY:       
-      ddnsd_log(p.uid, "killentry request");
       ddnsd_killentry(&p);
       break;
     default:
+      dump_packet(&p, "unsupported type/command");
       ddnsd_send_err(p.uid, DDNS_T_EUNSUPPTYPE, "unsupported type/command");
     }
   
-  exit(0);
+  return 0;
 }
